8a22afeba9168810bcb811e9043f0a844613464d
Ivan <ivan@Katana.Ivan>
2026-01-28 18:13:13 +0300

    add bitbucket pipelines, rename AWS vars to S3

--- app/s3_storage.py
+++ app/s3_storage.py
@@ -39,9 +39,9 @@ class S3CheckpointStorage:
                 endpoint_url = os.getenv("S3_ENDPOINT_URL")
 
                 s3_config = {
-                    'aws_access_key_id': os.getenv("AWS_ACCESS_KEY_ID"),
-                    'aws_secret_access_key': os.getenv("AWS_SECRET_ACCESS_KEY"),
-                    'region_name': os.getenv("AWS_REGION", "us-east-1")
+                    'aws_access_key_id': os.getenv("S3_ACCESS_KEY_ID"),
+                    'aws_secret_access_key': os.getenv("S3_SECRET_ACCESS_KEY"),
+                    'region_name': os.getenv("S3_REGION", "eu-central")
                 }
 
                 # Для S3-совместимых хранилищ (Yandex, MinIO)
